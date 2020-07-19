2d8bd19ff19dab5ee2a0aa06743cd6664dd4529e
Nate <37554478+servusDei2018@users.noreply.github.com>
2020-07-19 03:20:17 -0400

    Update stayalive.py

--- stayalive.py
+++ stayalive.py
@@ -3,12 +3,18 @@ import threading
 
 app = flask.Flask('Github Follower Bot')
 
+# Flask configuration
+host = {
+  'ip': '0.0.0.0',
+  'port': 3000
+}
+
 @app.route('/', methods=['GET','POST'])
 def home():
   return 'pong'
 
 def start():
-  app.run(host="0.0.0.0", port=3000)
+  app.run(host=host['ip'], port=host['port'])
 
 # thread = threading.Thread(target=start)
 # thread.start()
